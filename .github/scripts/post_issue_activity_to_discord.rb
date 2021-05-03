5fb1237eb22694c265a76e458597c723f308a8c4
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-05-03 09:50:08 -0400

    Parse command line args properly

--- .github/scripts/post_issue_activity_to_discord.rb
+++ .github/scripts/post_issue_activity_to_discord.rb
@@ -1,1 +1,18 @@
-puts "Hello"
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.on("-tTYPE", "--type=TYPE") do |v|
+    options[:type] = v
+  end
+
+  opts.on("-uURL", "--url=URL") do |v|
+    options[:url] = v
+  end
+
+  opts.on("-nName", "--name=NAME") do |v|
+    options[:name] = v
+  end
+end.parse!
+
+puts options
